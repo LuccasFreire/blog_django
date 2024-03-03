0c84456eff494f0998b2253af7bd83a0042c303b
Lucas Freire <lucasfreiredev1@gmail.com>
2024-03-03 01:04:37 -0300

    Created post model and app, created PostModelTest test

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+  titulo = models.CharField(max_length = 50)
+  corpo = models.TextField()
+  criado = models.DateTimeField(auto_now_add = True)
+  editado = models.DateTimeField(auto_now = True)
